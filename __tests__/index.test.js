d04088999940a4b7a1639bca2b6f05a3941ff17f
Ales <aleswebgit@gmail.com>
2022-03-13 15:01:15 +0100

    test and function

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,37 @@
-const fizzbuzz = require ('../src/index.js');
+const fizzbuzz = require ('../src/index.js')
 
+test('If number is divisible for 3 return Fizz', () => {
+  // given
+  let number = 3
+  // when
+  let result = fizzbuzz(number)
+  // then
+  expect(result).toBe('Fizz')
+})
 
+test('If number is divisible for 5 return Buzz', () => {
+  // given
+  let number = 5
+  // when
+  let result = fizzbuzz(number)
+  // then
+  expect(result).toBe('Buzz')
+})
 
+test('If number is divisible for 3 and 5 return FizzBuzz', () => {
+  // given
+  let number = 15
+  // when
+  let result = fizzbuzz(number)
+  // then
+  expect(result).toBe('FizzBuzz')
+})
+
+test('If number is not divisible for 3 or 5 return number', () => {
+  // given
+  let number = 7
+  // when
+  let result = fizzbuzz(number)
+  // then
+  expect(result).toBe(number)
+})
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 function fizzbuzz(number) {
-
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'FizzBuzz'
+  }
+  if (number % 3 === 0) {
+    return 'Fizz'
+  }
+  if (number % 5 === 0) {
+    return 'Buzz'
+  }
+  return number
 }
 
 
-module.exports = fizzbuzz;
+module.exports = fizzbuzz
